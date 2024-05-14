efb9f7e159d6515071fde804bbd47c39de4665b1
Ahmad Fahmid <lovelyyeasminjesmin@gmail.com>
2024-05-14 12:04:01 +0600

    Update homeSlice.js

--- src/redux/homeSlice.js
+++ src/redux/homeSlice.js
@@ -3,18 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const homeSlice = createSlice({
   name: "home",
   initialState: {
-    docSpecialization: "",
-    docName: "",
+    doctors: [],  // Assuming an array of doctors
   },
   reducers: {
     setDocDataFromHome: (state, action) => {
-      state.docSpecialization = action.payload.docSpecialization;
-      state.docName = action.payload.docName;
+      state.doctors = action.payload;  // Payload is an array of doctors with locations
     },
   },
 });
 
 export const { setDocDataFromHome } = homeSlice.actions;
-// export const selectDoctorData = (state) => state.doctor.data;
 
 export default homeSlice.reducer;
